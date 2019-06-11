126d8584298cad1804a588df56bdde0ddfdc1906
Mathias Müller <mathias.mueller@uzh.ch>
2019-06-11 14:33:18 +0200

    download base model instead of retraining

---
+++ scripts/download_baseline_model.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+scripts=`dirname "$0"`
+base=$scripts/..
+
+mkdir -p $base/models
+
+wget https://files.ifi.uzh.ch/cl/archiv/2019/mt19/mt19_u6_model.tar.gz -P $models
+
+tar -xzvf $models/mt19_u6_model.tar.gz
+
+mv $models/mt19_u6_model $models/model_wmt17_baseline
