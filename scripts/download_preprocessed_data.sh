06c8ed87d2a746562ce85bfbbf1c284bd4a237de
Mathias Müller <mathias.mueller@uzh.ch>
2019-06-11 15:07:59 +0200

    download preprocessed data

--- scripts/download_preprocessed_data.sh
+++ scripts/download_preprocessed_data.sh
@@ -7,6 +7,8 @@ base=$scripts/..
 
 data=$base/data
 
+mkdir -p $data
+
 wget https://files.ifi.uzh.ch/cl/archiv/2019/mt19/mt19_u6_model.tar.gz -P $data
 
 tar -xzvf $data/mt19_u6_model.tar.gz -C $data
