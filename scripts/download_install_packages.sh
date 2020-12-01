56367c8ab1d9c87a33dd52eb6086752a276e3367
Mathias Müller <mathias.mueller@uzh.ch>
2020-12-01 09:37:39 +0100

    fix install

--- scripts/download_install_packages.sh
+++ scripts/download_install_packages.sh
@@ -10,7 +10,9 @@ mkdir -p $tools
 
 # install Moses scripts for preprocessing
 
-git clone https://github.com/bricksdont/moses-scripts $tools/moses-scripts
+if [[ ! -d $tools/moses-scripts ]]; then
+    git clone https://github.com/bricksdont/moses-scripts $tools/moses-scripts
+fi
 
 # CUDA version on instance
 CUDA_VERSION=102
