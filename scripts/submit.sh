6bf661b232673f9d299bc78efd1a8b2b6de3b7b2
Mathias Müller <mathias.mueller@uzh.ch>
2020-12-04 11:57:06 +0100

    try from scratch, sth wrong with restart

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -12,6 +12,6 @@ mkdir -p $logs
 SLURM_DEFAULT_FILE_PATTERN="slurm-%j.out"
 SLURM_LOG_ARGS="-o $logs/$SLURM_DEFAULT_FILE_PATTERN -e $logs/$SLURM_DEFAULT_FILE_PATTERN"
 
-SLURM_ARGS="--qos=vesta --time=72:00:00 --gres gpu:Tesla-V100-32GB:1 --cpus-per-task 1 --mem 16g"
+SLURM_ARGS="--qos=vesta --time=168:00:00 --gres gpu:Tesla-V100-32GB:1 --cpus-per-task 1 --mem 16g"
 
 sbatch $SLURM_ARGS $SLURM_LOG_ARGS $scripts/all.sh $base
